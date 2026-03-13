f8d8eea997fc712d00bda2a3f9b31cdedd88f317
Lendariofull <victorpazsales@gmail.com>
2026-03-13 11:46:12 -0300

    correction datasource

--- src/database/datasource.ts
+++ src/database/datasource.ts
@@ -3,17 +3,23 @@ import { DataSource } from "typeorm";
 import { NotasCorte } from "../Entities/NotasCorte"; 
 import { Usuario } from "../Entities/Usuario"; 
 import { UsuarioConfig } from "../Entities/UsuarioConfig"; 
+import "dotenv/config";
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres", 
-    password: "admin",
-    database: "NotasDeCortes",
+    host: process.env.TYPEORM_HOST as string,
+    port: Number(process.env.TYPEORM_PORT) || 5432,
+    username: process.env.TYPEORM_USERNAME as string,
+    password: process.env.TYPEORM_PASSWORD as string,
+    database: process.env.TYPEORM_DATABASE as string,
     synchronize: false, 
     logging: true,    
     entities: [NotasCorte, Usuario, UsuarioConfig],
     migrations: [],
     subscribers: [],
+    extra: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
 })
